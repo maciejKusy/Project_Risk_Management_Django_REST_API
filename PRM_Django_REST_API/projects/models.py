c9a677a9809399127ed167d21f92ac939ccfbc37
MKurtz666 <mkusy936@gmail.com>
2022-01-11 20:01:06 +0100

    implemented JWT token authorization, alongisde session authorization

--- PRM_Django_REST_API/projects/models.py
+++ PRM_Django_REST_API/projects/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=300)
-    users_assigned = models.ManyToManyField(User)
+    users_assigned = models.ManyToManyField(User, blank=False)
 
     def __str__(self):
         return self.name
--- PRM_Django_REST_API/projects/views.py
+++ PRM_Django_REST_API/projects/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from .models import Project, Risk
 from .serializers import ProjectSerializer, DetailedRiskSerializer
@@ -6,8 +7,10 @@ from .serializers import ProjectSerializer, DetailedRiskSerializer
 class ProjectView(ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class RiskView(ModelViewSet):
     serializer_class = DetailedRiskSerializer
     queryset = Risk.objects.all()
+    permission_classes = [IsAuthenticated]
