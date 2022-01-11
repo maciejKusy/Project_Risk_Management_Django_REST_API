2c6c802b4b22a51508cf75a862366ba94a834f00
MKurtz666 <mkusy936@gmail.com>
2022-01-11 21:10:30 +0100

    Refined the serialization used for different endpoints. Possibly to be improved after further requirements are stated.

--- PRM_Django_REST_API/projects/serializers.py
+++ PRM_Django_REST_API/projects/serializers.py
@@ -4,6 +4,7 @@ from .models import Project, Risk
 
 class RiskSerializer(serializers.ModelSerializer):
     background = serializers.CharField(source='get_background_display')
+    user_assigned = serializers.StringRelatedField(many=False)
     priority = serializers.CharField(source='get_priority_display')
     probability_percentage = serializers.CharField(source='get_probability_percentage_display')
 
@@ -14,22 +15,24 @@ class RiskSerializer(serializers.ModelSerializer):
 
 class ProjectSerializer(serializers.ModelSerializer):
     risks = RiskSerializer(many=True, read_only=True)
+    users_assigned = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Project
-        fields = ['name', 'description', 'risks']
+        fields = ['name', 'description', 'risks', 'users_assigned']
         depth = 1
 
 
 class DetailedRiskSerializer(serializers.ModelSerializer):
     project = serializers.StringRelatedField(many=False)
     background = serializers.CharField(source='get_background_display')
+    user_assigned = serializers.StringRelatedField(many=False)
     priority = serializers.CharField(source='get_priority_display')
     probability_percentage = serializers.CharField(source='get_probability_percentage_display')
 
     class Meta:
         model = Risk
-        fields = ['name', 'project', 'background', 'user_assigned', 'priority', 'probability_percentage']
+        fields = ['id', 'name', 'project', 'background', 'user_assigned', 'priority', 'probability_percentage']
 
 
 
