56816651c9a8bb01d696b95f2f0ba59140f0dea0
MKurtz666 <mkusy936@gmail.com>
2022-01-16 21:26:04 +0100

    Added a serializer for the user profiles - enables the Projects and Risks endpoints to provide a serialized version of the profiles of users assigned to projects and risks

--- PRM_Django_REST_API/projects/models.py
+++ PRM_Django_REST_API/projects/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.datetime_safe import datetime
 from simple_history.models import HistoricalRecords
+from users.models import Profile
 
 
 class Project(models.Model):
@@ -23,6 +25,7 @@ class Risk(models.Model):
         HIGH = '3', 'High'
 
     class Probability(models.TextChoices):
+        ZERO_PERCENT = '0', '0%'
         TEN_PERCENT = '1', '10%'
         TWENTY_PERCENT = '2', '20%'
         THIRTY_PERCENT = '3', '30%'
@@ -37,12 +40,30 @@ class Risk(models.Model):
     name = models.CharField(max_length=100, blank=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='risks', blank=False)
     background = models.CharField(max_length=50, choices=Background.choices, blank=False)
-    user_assigned = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
-    priority = models.CharField(max_length=10, choices=Priority.choices, blank=False)
-    probability_percentage = models.CharField(max_length=6, choices=Probability.choices, blank=False)
+    user_assigned = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
+    priority = models.CharField(max_length=2, choices=Priority.choices, blank=False)
+    probability_percentage = models.CharField(max_length=2, choices=Probability.choices, blank=False)
     change_history = HistoricalRecords()
 
     def __str__(self):
         return self.name
+
+    @property
+    def get_change_history(self):
+        history = self.change_history.all().values()
+        changes_list = list(history)
+        irrelevant_changes = ['history_id', 'history_date', 'history_type']
+        changes_descriptions = []
+        for index, change in enumerate(changes_list):
+            if index != 0:
+                for key, value in change.items():
+                    if changes_list[index - 1][key] != changes_list[index][key]:
+                        if key not in irrelevant_changes:
+                            new_value = changes_list[index - 1][key]
+                            old_value = changes_list[index][key]
+                            timestamp = datetime.strftime(changes_list[index]['history_date'], '%d-%m-%Y, %H:%M:%S')
+                            changes_descriptions.append(f'Change: {key} was changed from {old_value} to {new_value}'
+                                                        f' on {timestamp}.')
+        return changes_descriptions
 
 
--- PRM_Django_REST_API/projects/serializers.py
+++ PRM_Django_REST_API/projects/serializers.py
@@ -1,59 +1,42 @@
-from django.utils.datetime_safe import datetime
 from rest_framework import serializers
 from .models import Project, Risk
+from users.serializers import UserProfileSerializer
 
 
 class RiskSerializer(serializers.ModelSerializer):
     background = serializers.CharField(source='get_background_display')
-    user_assigned = serializers.StringRelatedField(many=False)
+    user_assigned = UserProfileSerializer(many=False, read_only=True)
     priority = serializers.CharField(source='get_priority_display')
     probability_percentage = serializers.CharField(source='get_probability_percentage_display')
 
     class Meta:
         model = Risk
-        fields = ['name', 'background', 'user_assigned', 'priority', 'probability_percentage']
+        fields = ['id', 'name', 'background', 'user_assigned', 'priority', 'probability_percentage']
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     risks = RiskSerializer(many=True, read_only=True)
-    users_assigned = serializers.StringRelatedField(many=True)
+    users_assigned = UserProfileSerializer(many=True, read_only=True)
 
     class Meta:
         model = Project
-        fields = ['name', 'description', 'risks', 'users_assigned']
-        depth = 1
+        fields = ['id', 'name', 'description', 'risks', 'users_assigned']
 
 
 class DetailedRiskSerializer(serializers.ModelSerializer):
     project = serializers.StringRelatedField(many=False)
     background = serializers.CharField(source='get_background_display')
-    user_assigned = serializers.StringRelatedField(many=False)
+    user_assigned = UserProfileSerializer(many=False, read_only=True)
     priority = serializers.CharField(source='get_priority_display')
     probability_percentage = serializers.CharField(source='get_probability_percentage_display')
-    change_history = serializers.SerializerMethodField(read_only=True)
+    change_history = serializers.ListField(source='get_change_history')
 
     class Meta:
         model = Risk
         fields = ['id', 'name', 'project', 'background', 'user_assigned', 'priority', 'probability_percentage',
                   'change_history']
 
-    @classmethod
-    def get_change_history(cls, instance):
-        history = instance.change_history.all().values()
-        changes_list = list(history)
-        irrelevant_changes = ['history_id', 'history_date']
-        changes_descriptions = []
-        for index, change in enumerate(changes_list):
-            if index != 0:
-                for key, value in change.items():
-                    if changes_list[index-1][key] != changes_list[index][key]:
-                        if key not in irrelevant_changes:
-                            new_value = changes_list[index - 1][key]
-                            old_value = changes_list[index][key]
-                            timestamp = datetime.strftime(changes_list[index]['history_date'], '%d-%m-%Y, %H:%M:%S')
-                            changes_descriptions.append(f'Change: {key} was changed from {old_value} to {new_value}'
-                                                        f' on {timestamp}.')
-        return changes_descriptions
+
 
 
 
